63698409658e97cd21c8a6f062d38ac471803963
pavittarsingh315 <pavittarsingh12364@gmail.com>
2022-07-04 21:59:52 -0700

    Made profile tab bodies into CustomScrollViews so that they can maintain scroll position.

--- lib/pages/profile/components/private_posts.dart
+++ lib/pages/profile/components/private_posts.dart
@@ -66,25 +66,34 @@ class _PrivatePostsBodyState extends State<PrivatePostsBody> {
 
   @override
   Widget build(BuildContext context) {
-    return MediaQuery.removePadding(
-      context: context,
-      removeTop: true,
-      child: ListView.builder(
-        itemCount: 69,
-        padding: EdgeInsets.only(bottom: navBarHeight(context)),
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 50,
-            margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 11),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: index % 2 == 0 ? Colors.green.shade100 : Colors.pink.shade100,
-              borderRadius: BorderRadius.circular(11),
+    return Builder(
+      builder: (context) {
+        return CustomScrollView(
+          key: const PageStorageKey("private_posts"),
+          slivers: [
+            SliverPadding(
+              padding: EdgeInsets.only(bottom: navBarHeight(context)),
+              sliver: SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    return Container(
+                      height: 50,
+                      margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 11),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: index % 2 == 0 ? Colors.green.shade100 : Colors.pink.shade100,
+                        borderRadius: BorderRadius.circular(11),
+                      ),
+                      child: Text(index.toString()),
+                    );
+                  },
+                  childCount: 69,
+                ),
+              ),
             ),
-            child: Text(index.toString()),
-          );
-        },
-      ),
+          ],
+        );
+      },
     );
   }
 }
--- lib/pages/profile/components/public_posts.dart
+++ lib/pages/profile/components/public_posts.dart
@@ -26,25 +26,34 @@ class _PublicPostsState extends State<PublicPosts> {
 
   @override
   Widget build(BuildContext context) {
-    return MediaQuery.removePadding(
-      context: context,
-      removeTop: true,
-      child: ListView.builder(
-        itemCount: 69,
-        padding: EdgeInsets.only(bottom: navBarHeight(context)),
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 50,
-            margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 11),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: index % 2 == 0 ? Colors.orange.shade200 : Colors.purple.shade100,
-              borderRadius: BorderRadius.circular(11),
-            ),
-            child: Text(index.toString()),
-          );
-        },
-      ),
+    return Builder(
+      builder: (context) {
+        return CustomScrollView(
+          key: const PageStorageKey<String>("public_posts"),
+          slivers: [
+            SliverPadding(
+              padding: EdgeInsets.only(bottom: navBarHeight(context)),
+              sliver: SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    return Container(
+                      height: 50,
+                      margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 11),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: index % 2 == 0 ? Colors.orange.shade200 : Colors.purple.shade100,
+                        borderRadius: BorderRadius.circular(11),
+                      ),
+                      child: Text(index.toString()),
+                    );
+                  },
+                  childCount: 69,
+                ),
+              ),
+            )
+          ],
+        );
+      },
     );
   }
 }
